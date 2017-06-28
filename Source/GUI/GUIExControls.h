454cf032f0f4377902b8288539963fca2644b298
Jasmine <jasmine.leblondc@gmail.com>
2017-06-27 22:03:43 -0400

    GUIExControls.h: void listeners setup

--- Source/GUI/GUIExControls.h
+++ Source/GUI/GUIExControls.h
@@ -12,38 +12,41 @@
 
 #include "../../JuceLibraryCode/JuceHeader.h"
 
-class GUIExControls : public Component
+class GUIExControls : public Component,
+                    public Button::Listener, public Slider::Listener, public ComboBox::Listener
 {
 public:
     GUIExControls()
     {
         addAndMakeVisible(freqrange);
-       // freqrange->setBounds (0.6*getWidth(), 0.15*getHeight(), 100, 50);
         freqrange->addItem("ALL", 1);
         freqrange->addSeparator();
         freqrange->addItem("High", 2);
         freqrange->addItem("Mid", 3);
         freqrange->addItem("Low", 4);
         freqrange->setSelectedId(1);
+        freqrange->addListener(this);
         
         addAndMakeVisible(dBAmpSlider);
         dBAmpSlider.setRange(3, 12, 3);
         dBAmpSlider.setTextValueSuffix("dB");
         dBAmpSlider.setSliderStyle(juce::Slider::LinearVertical);
         dBAmpSlider.setTextBoxIsEditable(false);
-       // dBAmpSlider.addListener(this);
+        dBAmpSlider.addListener(this);
         
         amplify.setButtonText("+");
-       // amplify.addListener(this);
+        amplify.addListener(this);
         addAndMakeVisible(amplify);
         
         attenuate.setButtonText("-");
+        attenuate.addListener(this);
         addAndMakeVisible(attenuate);
         
         addAndMakeVisible(volumeSlider);
         volumeSlider.setRange(1, 10, 1); //temporary range
         volumeSlider.setSliderStyle(juce::Slider::LinearVertical);
         volumeSlider.setTextBoxIsEditable(false);
+        volumeSlider.addListener(this);
     }
     
     ~GUIExControls()
@@ -67,6 +70,21 @@ public:
         
         volumeSlider.setBounds (0.6*getWidth(), 0.45*getHeight(), 30, 135);
     }
+    
+    void buttonClicked(Button* button) override
+    {
+    }
+    
+    void sliderValueChanged (Slider* slider) override
+    {
+        
+    }
+    
+    void comboBoxChanged (ComboBox* comboBoxThatHasChanged) override
+    {
+        
+    }
+    
     
 private:
     Slider dBAmpSlider;
